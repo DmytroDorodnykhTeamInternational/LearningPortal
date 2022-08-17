7306520b06def942e82f375bcb0da7567a241533
dangoth <alpha6667@gmail.com>
2022-08-17 19:32:41 +0200

    added error suppression and removed sleep from ps script

--- run.ps1
+++ run.ps1
@@ -1,10 +1,8 @@
-docker-compose up -d
+docker-compose up -d 2>$null
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 cd "$dir\Portal_Perfomance_Employes"
 dotnet user-secrets init
 dotnet user-secrets set "ConnectionStrings:EmployeeDbString" "Server=localhost,1433;Database=master;User ID=sa;Password=Password123!;"
-Start-Sleep -Seconds 2
-dotnet tool install --global dotnet-ef
-Start-Sleep -Seconds 2
+dotnet tool install --global dotnet-ef 2>$null
 dotnet ef database update
