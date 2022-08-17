e003b1bd0932c489567f22c68d8abbb3e925a3fb
dangoth <alpha6667@gmail.com>
2022-08-17 18:31:11 +0200

    added user secret creation and db update in ps script, removed vscode files

--- run.ps1
+++ run.ps1
@@ -1,1 +1,8 @@
 docker-compose up -d
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+cd "$dir\Portal_Perfomance_Employes"
+dotnet user-secrets init
+dotnet user-secrets set "ConnectionStrings:EmployeeDbString" "Server=localhost,1433;Database=master;User ID=sa;Password=Password123!;"
+Start-Sleep -Seconds 2
+dotnet ef database update
