a79a4bc53e2c701f9efa8ce288eca93e9a530a35
dangoth <alpha6667@gmail.com>
2022-08-17 19:13:39 +0200

    added entity framework install

--- run.ps1
+++ run.ps1
@@ -5,4 +5,6 @@ cd "$dir\Portal_Perfomance_Employes"
 dotnet user-secrets init
 dotnet user-secrets set "ConnectionStrings:EmployeeDbString" "Server=localhost,1433;Database=master;User ID=sa;Password=Password123!;"
 Start-Sleep -Seconds 2
+dotnet tool install --global dotnet-ef
+Start-Sleep -Seconds 2
 dotnet ef database update
