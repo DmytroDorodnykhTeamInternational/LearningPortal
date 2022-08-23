8785cfdf860ce704d5d718fde075943bf96f7735
Vladyslav Cherednychenko <vladislav.cherednychenko@gmail.com>
2022-08-23 18:04:12 +0300

    redesign of UserInfoController

--- Portal_Perfomance_Employes/Controllers/UserInfoController.cs
+++ Portal_Perfomance_Employes/Controllers/UserInfoController.cs
@@ -7,7 +7,7 @@ using PortalPerfomanceEmployees.Models;
 
 namespace PortalPerfomanceEmployees.Controllers
 {
-    [Route("ShowUserInfo")]
+    [Route("UserController")]
     public class UserInfoController : Controller
     {
         private readonly EmployeeContext _context;
@@ -18,18 +18,16 @@ namespace PortalPerfomanceEmployees.Controllers
         }
 
         [HttpGet]
-        [Route("{id}")]
-        public async Task<JsonResult> ShowUserInfo(int? id)
+        [Route("colleagues/")]
+        public async Task<JsonResult> ShowTeamInfo(int? id)
         {
             var emp = await _context.Employees
                 .FirstOrDefaultAsync(e => e.Id == id);
             if (emp == null)
-                return Json("Employee with that ID was not found");
-            if (emp.Role == Role.Employee)
             {
-                return Json(emp);
+                return Json("Employee with that ID was not found");
             }
-            else if (emp.Role == Role.Teamlead)
+            else if (emp.Role == Role.Teamlead || emp.Role == Role.Employee)
             {
                 var empList = await _context.Employees
                         .Where(e => e.TeamId == emp.TeamId)
@@ -41,6 +39,29 @@ namespace PortalPerfomanceEmployees.Controllers
                 return Json(await _context.Employees.ToListAsync());
             }
             return Json("Something went wrong");
+        }
+
+        [HttpGet]
+        [Route("user/")]
+        public async Task<JsonResult> ShowUserInfo(int? id, int? idOfInterest)
+        {
+            var user = await _context.Employees
+                .FirstOrDefaultAsync(e => e.Id == id);
+            var personOfInterest = await _context.Employees
+                .FirstOrDefaultAsync(e => e.Id == idOfInterest);
+            if (user == null || personOfInterest == null)
+            {
+                return Json("Employee with that ID was not found");
+            }
+            if (user.Role == Role.Admin)
+            {
+                return Json(personOfInterest);
+            }
+            if (user.TeamId == personOfInterest.TeamId)
+            {
+                return Json(personOfInterest);
+            }
+            return Json("Access denied. You do not have the appropriate permissions.");
         }
     }
 }
