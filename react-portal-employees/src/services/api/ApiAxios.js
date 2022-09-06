765a6ff2d647f007e5651c69a5158cfa7e7beac5
Ygoraphael <ygoraphael@gmail.com>
2022-09-06 06:07:10 +0100

    Change to inform that the request body is a json

--- react-portal-employees/src/services/api/ApiAxios.js
+++ react-portal-employees/src/services/api/ApiAxios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
+    headers: {'Content-Type' : 'application/json'},
     baseURL: 'https://localhost:7147/api',
     responsetype: 'json'
 });
